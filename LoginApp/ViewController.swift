bbd154b5f2fadade3ae41b1b8d2ce89b6ce43e67
Surgurator <a9191084222@gmail.com>
2022-12-08 23:13:42 +0300

    Исправления с учетом фидбека

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -8,54 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // MARK: - IBOtlets
+    // MARK: - IBOtlets and properties
     @IBOutlet var userNameTextF: UITextField!
     @IBOutlet var passwordTextF: UITextField!
     
+    private let userName = "User"
+    private let password = "Password"
+    
+    // MARK: - override func
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let welcomeController = segue.destination as? WelcomeViewController {
             welcomeController.userNameWelcome = String("Welcome \(userNameTextF.text!)")
         }
     }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
         view.endEditing(true)
     }
+    
     // MARK: - IBActions
     
     @IBAction func unwind(for segue: UIStoryboardSegue) {
-        guard let logOut = segue.destination as? ViewController else {return}
-        logOut.userNameTextF.text = ""
-        logOut.passwordTextF.text = ""
+        userNameTextF.text = ""
+        passwordTextF.text = ""
     }
     
     @IBAction func logInButton(_ sender: UIButton) {
-        if userNameTextF.text == "User", passwordTextF.text == "Password"{
+        if userNameTextF.text == userName, passwordTextF.text == password{
             performSegue(withIdentifier: "welcomeSegue", sender: nil)
         } else {
             alertLoginShow(
                 title: "Авторизация невозможна",
                 message: "Неверно введен логин или пароль")
-            }
-            passwordTextF.text = ""
         }
-    
+        passwordTextF.text = ""
+    }
     
     @IBAction func forgotUserNameButton() {
-        alertLoginShow(title: "Логин", message: "User")
+        alertLoginShow(title: "Логин", message: userName)
     }
     
     @IBAction func forgotPassButton() {
-        alertLoginShow(title: "Пароль", message: "Password")
+        alertLoginShow(title: "Пароль", message: password)
     }
     
-
-   private func alertLoginShow(title: String, message: String){
-     let showLogin = UIAlertController(
-        title: title,
-        message: message,
-        preferredStyle: .alert)
-    let buttonOK = UIAlertAction(title: "OK", style: .default)
-    showLogin.addAction(buttonOK)
-    present(showLogin, animated: true)
-}
+    private func alertLoginShow(title: String, message: String){
+        let showLogin = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert)
+        let buttonOK = UIAlertAction(title: "OK", style: .default)
+        showLogin.addAction(buttonOK)
+        present(showLogin, animated: true)
+    }
 }
