d9e05899afec16e77ae806c99ed3e78799d861fd
Surgurator <a9191084222@gmail.com>
2022-12-08 20:08:25 +0300

    доработки

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -32,43 +32,48 @@ class ViewController: UIViewController {
         if userNameTextF.text == "User", passwordTextF.text == "Password"{
             performSegue(withIdentifier: "welcomeSegue", sender: nil)
         } else {
-            func alertLoginPass(){
-                let alert = UIAlertController(
-                    title: "Авторизация невозможна",
-                    message: "Неверно введен логин или пароль",
-                    preferredStyle: .alert)
-                let buttonOK = UIAlertAction(title: "OK", style: .default)
-                alert.addAction(buttonOK)
-                self.present(alert, animated: true)
-            }
             alertLoginPass()
+            }
             passwordTextF.text = ""
         }
-    }
+    
     
     @IBAction func forgotUserNameButton() {
-        func alertLoginShow(){
-            let showLogin = UIAlertController(
-                title: "Логин",
-                message: "User",
-                preferredStyle: .alert)
-            let buttonOK = UIAlertAction(title: "OK", style: .default)
-            showLogin.addAction(buttonOK)
-            self.present(showLogin, animated: true)
-        }
         alertLoginShow()
     }
     
     @IBAction func forgotPassButton() {
-        func alertPassShow(){
-            let showPass = UIAlertController(
-                title: "Пароль",
-                message: "Password",
-                preferredStyle: .alert)
-            let buttonOK = UIAlertAction(title: "OK", style: .default)
-            showPass.addAction(buttonOK)
-            self.present(showPass, animated: true)
-        }
+
         alertPassShow()
     }
+    func alertLoginPass(){
+        let alert = UIAlertController(
+            title: "Авторизация невозможна",
+            message: "Неверно введен логин или пароль",
+            preferredStyle: .alert)
+        let buttonOK = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(buttonOK)
+        present(alert, animated: true)
+    }
+ 
+func alertLoginShow(){
+    let showLogin = UIAlertController(
+        title: "Логин",
+        message: "User",
+        preferredStyle: .alert)
+    let buttonOK = UIAlertAction(title: "OK", style: .default)
+    showLogin.addAction(buttonOK)
+    present(showLogin, animated: true)
+}
+
+    func alertPassShow(){
+        let showPass = UIAlertController(
+            title: "Пароль",
+            message: "Password",
+            preferredStyle: .alert)
+        let buttonOK = UIAlertAction(title: "OK", style: .default)
+        showPass.addAction(buttonOK)
+        present(showPass, animated: true)
+    }
+
 }
